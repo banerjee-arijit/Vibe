022c654d97dbdca1dc6518a8d7292a59157b0351
ArijitBanerjee <arijitbanerjee873@gmail.com>
2025-11-09 10:28:17 +0530

    first commit added client and server folder

---
+++ client/src/main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+
+if (!rootElement) throw new Error("Root Element not found");
+
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
